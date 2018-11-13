4bda89be1e8b28539a9226598575a1603527de9e
Rafael Moura <rafaelmoura.dev@gmail.com>
2018-11-13 00:46:30 -0200

    rotating

--- rotate/opengl/rotate.c
+++ rotate/opengl/rotate.c
@@ -1,14 +1,16 @@
+#include <cglm/cglm.h>
 #include <sdl2_opengl/sdl2_opengl.h>
 
 
 const GLchar* const vs_src =
 "#version 130\n"
-"in vec2 pos;\n"
+"in vec3 pos;\n"
 "in vec3 rgb;\n"
 "out vec4 frag_color;\n"
+"uniform mat4 trans;\n"
 "void main()\n"
 "{\n"
-"	gl_Position = vec4(pos, 0.0, 1.0);\n"
+"	gl_Position = trans * vec4(pos, 1.0);\n"
 "	frag_color = vec4(rgb, 1.0);\n"
 "}\n";
 
@@ -23,11 +25,33 @@ const GLchar* const fs_src =
 "}\n";
 
 
+struct vertex_data {
+	vec3 pos;
+	vec3 rgb;
+};
+
+
 int main(void)
 {
-	if (!sdl2_opengl_init("CUBE", 800, 600, vs_src, fs_src)) {
+	if (!sdl2_opengl_init("ROTATE", 800, 600, vs_src, fs_src)) {
 		return EXIT_FAILURE;
 	}
+
+	sdl2_opengl_vattrp("pos", 3, GL_FLOAT, GL_TRUE, sizeof(struct vertex_data), NULL);
+	sdl2_opengl_vattrp("rgb", 3, GL_FLOAT, GL_TRUE, sizeof(struct vertex_data),
+	                   (void*)((struct vertex_data*)NULL)->rgb);
+
+
+	mat4 trans = GLM_MAT4_IDENTITY_INIT;
+
+	sdl2_opengl_set_uniform("trans", trans);
+
+	struct vertex_data verts[] = {
+		{{-0.5, -0.5, 0}, {1, 0, 1}},
+		{{ 0.5, -0.5, 0}, {1, 0, 1}},
+		{{ 0.5,  0.5, 0}, {1, 0, 1}},
+		{{-0.5,  0.5, 0}, {1, 0, 1}}
+	};
 
 
 	while (sdl2_opengl_handle_events()) {
@@ -35,6 +59,12 @@ int main(void)
 		
 		glClearColor(0, 0, 0, 0xFF);
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		glm_rotate_z(trans, 0.05, trans);
+		sdl2_opengl_set_uniform("trans", trans);
+
+		glBufferData(GL_ARRAY_BUFFER, sizeof(verts), verts, GL_STREAM_DRAW);
+		glDrawArrays(GL_QUADS, 0, sizeof(verts)/sizeof(verts[0]));
 
 		sdl2_opengl_end_frame();
 	}
